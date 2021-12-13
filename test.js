aee53e761577e5ff1b220bd1a84aec31db1fdf65
Vishal Vikram Singh <singhvishalvikram14@gmail.com>
2021-12-13 14:03:45 +0530

    Update test.js

--- test.js
+++ test.js
@@ -6,26 +6,27 @@ const mk = new luna.MnemonicKey({
     '' // my mnemonic
   });
 
-// connect to mainnet
+// connect to soju testnet
 const terra = new luna.LCDClient({
   URL: 'https://lcd.terra.dev',
   chainID: 'columbus-4',
+  gasPrices: '0.15uluna'
 });
 
 const wallet = terra.wallet(mk);
 
 const send = new luna.MsgSend(
   'terra18sxn99tj3dtla7kqgexmawc4n258chk9sc4s2c',
-  'terra1ncjg4a59x2pgvqy9qjyqprlj8lrwshm0wleht5',
-  { uluna: 100, ukrw: 1230201, uusd: 1312029 }
+  'terra1ghxyl6fy4cerfcuh9hgcxjn7c7zkl08d9sn085',
+  { uluna: 100 }
 );
 
 wallet
   .createAndSignTx({
     msgs: [send],
-    memo: '100659208',
+    memo: 'hello',
   })
   .then(tx => terra.tx.broadcast(tx))
   .then(result => {
     console.log(`TX hash: ${result.txhash}`);
-  });
+  }).catch(console.error);
